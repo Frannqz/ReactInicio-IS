f9483d856fa0f9e19b484dd4de6068640f1330c8
Francisco Gonzalez Rodriguez <20030114@itcelaya.edu.mx>
2023-06-06 17:58:47 -0600

    JSX

--- src/index.js
+++ src/index.js
@@ -3,17 +3,49 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-//Primer componente
+//JSX: Combinacion de html y jsx
 function Greeting() {
+    const name = 'franco'
+    const married = true
+    //return <h1>{name}</h1>
+
+    // if (married) {
+    //     return <h1>Estoy en la escuela</h1>
+    // } else {
+    //     return <h1>No estoy en la escuela</h1>
+    // }
+    //Con  operador ternario ? = if
+    return <h1>{married ? 'Estoy en la escuela' : 'No estoy en la escuela'}</h1>
+}
+function Greeting2() {
+    const user = {
+        firstName: 'franqz',
+        lastName: 'Kafka'
+    }
+    // return <h1>{JSON.stringify(user)}</h1>
     return <div>
-        <h1>Hola mundo</h1>
+        <h1>{user.firstName}</h1>
+        <h3>{user.lastName}</h3>
     </div>
 }
+function Suma() {
+    function add(x, y) {
+        return x + y;
+    }
+
+    return (<div>
+        <h1>{add(10, 30)}</h1>
+    </div>);
+}
+
+
 
 root.render(
     <div>
         <Greeting />
-        <Greeting />
+        <Greeting2 />
+        <Suma />
     </div>
 )
+
 
